5a724b2067015fc5710e4f7de04a86c0a75f2d4d
mrsubhojitdhali-ops <mr.subhojitdhali@gmail.com>
2026-09-05 22:04:12 +0530

    fix cors duplicate error

--- Backend/server.js
+++ Backend/server.js
@@ -2,16 +2,17 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
-const seedInspector = require('./seed'); // 1. eta add koro
+const seedInspector = require('./seed');
 
 dotenv.config();
 
 const app = express();
-const cors = require('cors');
+
 app.use(cors({
-  origin: ["http://localhost:5173", "https://tomar-project.vercel.app"],
+  origin: ["http://localhost:5173", "https://techtonic-final.vercel.app", "https://techtonic-final.onrender.com"],
   credentials: true
 }));
+
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/authRoutes'));
@@ -21,12 +22,11 @@ app.get('/', (req, res) => res.send('API Running'));
 
 const PORT = process.env.PORT || 5000;
 
-// 2. Start function change koro
 const startServer = async () => {
   try {
     await connectDB();
     console.log("MongoDB Connected");
-    await seedInspector(); // Server start holei inspector DB te chole jabe
+    await seedInspector();
     app.listen(PORT, () => console.log(`Server running on ${PORT}`));
   } catch (err) {
     console.log("Server start error:", err);
